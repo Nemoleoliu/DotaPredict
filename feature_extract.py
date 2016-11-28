8ec05e9ab6d5420a89c1b557caafe9901ec89b97
Nemoleoliu <nemo.leo.liu@gmail.com>
2016-11-28 14:31:48 -0500

    Add basic functions

---
+++ feature_extract.py
@@ -0,0 +1,9 @@
+
+
+
+'''
+input: count
+return: 217 * count boolean array
+'''
+def feature_vector_extract(count):
+    pass
---
+++ predict.py
@@ -0,0 +1,15 @@
+from perf_output import *
+from feature_extract import *
+
+def main():
+    count = 1000
+    dataset = feature_vector_extract(count)
+    X_train, X_test, Y_train, Y_test = ...
+    # Use logisitic regression to predict
+
+    # Test
+
+    # perf_output
+
+if __name__ == '__main__':
+    main()
