8a6ca241b9b509287e010c62cb18ddb9fe33850e
Nemoleoliu <nemo.leo.liu@gmail.com>
2016-11-28 14:53:23 -0500

    Simple logisitic regression model

--- predict.py
+++ predict.py
@@ -1,21 +1,20 @@
 from perf_output import *
 from feature_extract import *
 from sklearn.model_selection import train_test_split
+from sklearn import linear_model
 
 def main():
     count = 1000
-    dataset = feature_vector_extract(count)
-    data_train, data_test = train_test_split(dataset);
-
-    data_train_X = data_train.iloc[:, predictor];
-    data_train_y = data_train['MEDV'];
-    data_test_X = data_test.iloc[:, predictor];
-    data_test_y = data_test['MEDV'];
+    dataset_X, dataset_y = feature_vector_extract(count)
+    X_train, X_test, y_train, y_test = train_test_split(dataset_X, dataset_y)
     regr = linear_model.LinearRegression();
-    X_train, X_test, Y_train, Y_test = ...
     # Use logisitic regression to predict
+    regressionFunc = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+    train_sco=regressionFunc.fit(X_train,y_train).score(X_train,y_train)
+
 
     # Test
+    test_sco=regressionFunc.score(X_test,y_test)
 
     # perf_output
 
